2a69da0dfad978af9133e14cf3615a41f167ac31
tiago-rangel <tiagorng@hotmail.com>
2023-07-05 13:54:58 -0300

    Teste Python

---
+++ MenuProtheus/menu.py
@@ -0,0 +1,15 @@
+
+
+
+print('Este é um configurador de ambiente para o sistema Protheus. \n')
+print('Escolha uma das opções abaixo. \n')
+print('''[ 1 ] Padrão
+[ 2 ] MyMobility
+[ 3 ] MyMobility_1299
+[ 4 ] Estoque
+[ 5 ] Logística
+[ 6 ] Oficina
+[ 7 ] Safran Representações
+[ 8 ] Ambiente de Validação
+[ 9 ] Sair do configurador''')
+#opcao = str(input('Qual é a sua opção? '))
