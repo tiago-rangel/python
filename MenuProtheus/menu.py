d2f74b19f9dfb8e955baec5b21f734e6cc27e000
tiago-rangel <tiagorng@hotmail.com>
2023-07-06 19:52:14 -0300

    Update menu.py

--- MenuProtheus/menu.py
+++ MenuProtheus/menu.py
@@ -1,4 +1,7 @@
 import os
+import configparser
+config = configparser.ConfigParser()
+config.read("C:\Program Files\Protheus_2210\smartclient.ini")
 os.system('cls')
 opcao = 0
 while opcao != 9:
@@ -17,57 +20,41 @@ while opcao != 9:
     print('**********************************************************\n')
     opcao = int(input('Qual é a sua opção? '))
     if opcao == 1:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.12')
         config.set('tcp', 'port', '1111')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 2:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.12')
         config.set('tcp', 'port', '1240')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 3:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.12')
         config.set('tcp', 'port', '1299')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 4:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.12')
         config.set('tcp', 'port', '1263')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 5:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.30')
         config.set('tcp', 'port', '1301')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 6:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.30')
         config.set('tcp', 'port', '1302')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 7:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.12')
         config.set('tcp', 'port', '1262')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
             config.write(configfile)
     elif opcao == 8:
-        import configparser
-        config = configparser.ConfigParser()
         config.set('tcp', 'server', '10.14.115.31')
         config.set('tcp', 'port', '1456')
         with open('C:\Program Files\Protheus_2210\smartclient.ini', 'w') as configfile:
