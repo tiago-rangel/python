e6fafe587e29d07a7f1dc3e43eb3aa3f61a92a6e
tiago-rangel <tiagorng@hotmail.com>
2023-07-09 13:55:05 -0300

    Projeto Menu (Update 09/07/2023)

--- MenuProtheus/menu.py
+++ MenuProtheus/menu.py
@@ -1,9 +1,113 @@
+import ctypes
+import win32security
+import ntsecuritycon as con
 import configparser
 import sys
+import time
 import os
 
+#O trecho abaixo verifica se o Protheus está devidamente instalado no computador.
 os.system('cls')
-# Este trecho de código realiza a primeira leitura do arquivo
+
+if not os.path.exists(r'C:\Program Files\Protheus_2210\smartclient.ini'):
+    print('\033[31m' + 'O Protheus não está corretamente instalado neste computador' + '\033[0m')
+    time.sleep(4)
+    sys.exit()
+    
+#trecho que limpa a tela e é informado o caminho do arquivo para verificação
+os.system('cls')
+arquivo = "C:\\Program Files\\Protheus_2210\\smartclient.ini"
+
+# Verificando se o programa está sendo executado por um usuário com privilégios de administrador
+
+if ctypes.windll.shell32.IsUserAnAdmin():
+   
+    # Obtendo o SID dos usuários
+    
+    try:
+        todos, domain, type = win32security.LookupAccountName("", "Todos")
+    except:
+        todos, domain, type = win32security.LookupAccountName("", "Everyone")
+    try:
+        usuarios, domain, type = win32security.LookupAccountName("", "Usuários")
+    except:
+        usuarios, domain, type = win32security.LookupAccountName("", "Users")
+
+    # Obtendo as informações de segurança do arquivo
+    
+    sd = win32security.GetFileSecurity(arquivo, win32security.DACL_SECURITY_INFORMATION)
+
+    # Obtendo a DACL atual do arquivo
+    
+    dacl = sd.GetSecurityDescriptorDacl()
+
+    # Verificando se os usuários já possuem as permissões solicitadas
+    
+    todos_permissoes = False
+    usuarios_permissoes = False
+    
+    for i in range(dacl.GetAceCount()):
+        ace = dacl.GetAce(i)
+        if ace[2] == todos:
+            todos_permissoes = True
+        elif ace[2] == usuarios:
+            usuarios_permissoes = True
+    
+    if not (todos_permissoes and usuarios_permissoes):
+        
+        # Adicionando os usuários com permissão de modificar, ler e executar, leitura, gravação e permissões especiais
+        
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_GENERIC_READ | con.FILE_GENERIC_WRITE | con.FILE_GENERIC_EXECUTE | con.DELETE | con.WRITE_DAC | con.WRITE_OWNER, todos)
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_GENERIC_READ | con.FILE_GENERIC_WRITE | con.FILE_GENERIC_EXECUTE | con.DELETE | con.WRITE_DAC | con.WRITE_OWNER, usuarios)
+
+        # Atualizando as informações de segurança do arquivo
+        
+        sd.SetSecurityDescriptorDacl(1, dacl, 0)
+        win32security.SetFileSecurity(arquivo, win32security.DACL_SECURITY_INFORMATION, sd)
+
+        # Exibindo a mensagem na cor verde utilizando ANSI
+        
+        print("\033[32mPermissões do arquivo alteradas\033[0m")
+        time.sleep(3)
+else:
+    # Verificando se o arquivo já foi previamente alterado
+    sd = win32security.GetFileSecurity(arquivo, win32security.DACL_SECURITY_INFORMATION)
+    dacl = sd.GetSecurityDescriptorDacl()
+    
+    try:
+        todos, domain, type = win32security.LookupAccountName("", "Todos")
+    except:
+        todos, domain, type = win32security.LookupAccountName("", "Everyone")
+    
+    try:
+        usuarios, domain, type = win32security.LookupAccountName("", "Usuários")
+    except:
+        usuarios, domain, type = win32security.LookupAccountName("", "Users")
+    
+    todos_permissoes = False
+    usuarios_permissoes = False
+    
+    for i in range(dacl.GetAceCount()):
+        ace = dacl.GetAce(i)
+        if ace[2] == todos:
+            todos_permissoes = True
+        elif ace[2] == usuarios:
+            usuarios_permissoes = True
+    
+    if not (todos_permissoes and usuarios_permissoes):
+        # Exibindo a mensagem na cor vermelha utilizando ANSI
+        print("\033[31mPara que o programa funcione favor executá-lo primeiro como administrador\033[0m")
+        
+        # Pausando a execução do programa por 3 segundos
+        time.sleep(3)
+        
+        # Encerrando o programa
+        sys.exit()
+
+os.system('cls')
+
+# Este trecho de código realiza novamente a leitura do arquivo para executar as tarefas de alteração conforme a opção do menu
+
 file_path = "C:\\Program Files\\Protheus_2210\\smartclient.ini"
 config = configparser.ConfigParser()
 config.read(file_path)
@@ -173,3 +277,6 @@ while True:
             print('\033[91mOpção inválida. Tente novamente.\033[0m')
     except ValueError:
         print('\033[91mEntrada inválida. Tente novamente.\033[0m')
+        input("pressione ENTER para sair")
+        
+        #Programa desemvolvido por Tiago Ribeiro Rangel
