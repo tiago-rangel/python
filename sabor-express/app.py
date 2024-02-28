ab4236fd147f6b522b1cedcc7cda9990d76d4be9
Tiago R. Rangel <87381423+tiago-rangel@users.noreply.github.com>
2024-02-27 21:01:33 -0300

    Create app.py

---
+++ sabor-express/app.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+print('\nSabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('2. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input ('Escolha uma opção ')
+print('\nVoce escolheu a opção', opcao_escolhida)
