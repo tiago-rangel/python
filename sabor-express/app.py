26c547b70c7ff733fd4a951833d77108d8bfa954
Tiago R. Rangel <87381423+tiago-rangel@users.noreply.github.com>
2024-03-04 21:44:34 -0300

    Update app.py

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -11,9 +11,20 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('2. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input ('Escolha uma opção ')
-print('\nVoce escolheu a opção', opcao_escolhida)
+# opcao_escolhida = input ('Escolha uma opção ')
+# print(f'Voce escolheu a opção {opcao_escolhida}')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print('cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurante')
+elif opcao_escolhida == 3:
+    print('Listar Ativar Restaurante')
+else:
+    print('Encerrando o programa')
